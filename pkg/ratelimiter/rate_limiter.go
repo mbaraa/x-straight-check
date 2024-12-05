bccec5b651889b9b87df3ab51c9310e8281c0c48
Baraa Al-Masri <pub@mbaraa.com>
2024-12-06 01:25:18 +0300

    Update saved responses

--- pkg/ratelimiter/rate_limiter.go
+++ pkg/ratelimiter/rate_limiter.go
@@ -1,9 +1,11 @@
 package ratelimiter
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"time"
 	"x-straight-check/cache"
+	"x-straight-check/log"
 	"x-straight-check/pkg/gemini"
 	"x-straight-check/pkg/xscraper"
 )
@@ -12,22 +14,32 @@ func GetUserAnalysis(username, lang string) (*gemini.UserStraightnessAnalysis, e
 	var res *gemini.UserStraightnessAnalysis
 	key := username + "#" + lang
 
-	cachedResp, err := cache.Get(key)
+	cachedRes, err := cache.Get(key)
 	if err == nil {
-		err = json.Unmarshal([]byte(cachedResp), res)
+		decRes, err := base64.StdEncoding.DecodeString(cachedRes)
 		if err != nil {
-			return nil, err
+			log.Errorf("Failed decoding cached response, %v\n", err)
+			goto noResp
+		}
+
+		err = json.Unmarshal([]byte(decRes), res)
+		if err != nil {
+			log.Errorf("Failed unmarshalling decoded cached response, %v\n", err)
+			goto noResp
 		}
 		return res, nil
 	}
+noResp:
 
 	posts, user, err := xscraper.GetUserPosts(username, lang, 35)
 	if err != nil {
+		log.Errorf("X Scrapper failed, %v\n", err)
 		return nil, err
 	}
 
 	res, err = gemini.CheckUserStraightness(posts, user)
 	if err != nil {
+		log.Errorf("AI failed, %v\n", err)
 		return nil, err
 	}
 
@@ -36,7 +48,9 @@ func GetUserAnalysis(username, lang string) (*gemini.UserStraightnessAnalysis, e
 		return nil, err
 	}
 
-	err = cache.SetWithTTL(key, string(resJson), time.Hour*3)
+	encResJson := base64.StdEncoding.EncodeToString(resJson)
+
+	err = cache.SetWithTTL(key, string(encResJson), time.Hour*3)
 	if err != nil {
 		return nil, err
 	}
